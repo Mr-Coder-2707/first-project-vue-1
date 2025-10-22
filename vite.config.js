6bef4708044626d0fc1e42daa991e2408af3e8b7
Mahmoud Sabry <mahmoudelkhwass704@gmail.com>
2025-10-22 19:50:28 +0300

    رفع المشروع الجديد

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/first-project-vue-1/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
